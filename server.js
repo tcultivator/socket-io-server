7b05cbaf35d66e2ec467472834c22c3516530f45
tcultivator <whypanahon06@gmail.com>
2025-11-03 23:01:15 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: { origin: "*" },
+});
+io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+
+    socket.on("QrCodeScan", (data) => {
+        console.log("QR data received:", data);
+        socket.broadcast.emit("update-data", data);
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+httpServer.listen(PORT, () => console.log(`Socket.IO server running on ${PORT}`));
